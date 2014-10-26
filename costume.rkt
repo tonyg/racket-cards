7244688a50a25c144ef2222e395330020290c941
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-10-26 16:47:59 -0400

    Terser rectangle costume

--- costume.rkt
+++ costume.rkt
@@ -38,34 +38,27 @@
     (init-field top-left bottom-right pen brush)
     (super-new)
 
-    (define/override {draw self dc}
+    (define (compute-corners)
       (define x1 (@ top-left x))
       (define y1 (@ top-left y))
       (define x2 (@ bottom-right x))
       (define y2 (@ bottom-right y))
+      (values (min x1 x2) (min y1 y2) (max x1 x2) (max y1 y2)))
+
+    (define/override {draw self dc}
+      (define-values (x1 y1 x2 y2) (compute-corners))
       {set-pen dc (@ pen value)}
       {set-brush dc (@ brush value)}
-      {draw-rectangle dc
-		      (min x1 x2)
-		      (min y1 y2)
-		      (abs (- x2 x1))
-		      (abs (- y2 y1))})
+      {draw-rectangle dc x1 y1 (- x2 x1) (- y2 y1)})
 
     (define/override {costume-under self p}
-      (define x1 (@ top-left x))
-      (define y1 (@ top-left y))
-      (define x2 (@ bottom-right x))
-      (define y2 (@ bottom-right y))
-      (define l (min x1 x2))
-      (define r (max x1 x2))
-      (define t (min y1 y2))
-      (define b (max y1 y2))
+      (define-values (x1 y1 x2 y2) (compute-corners))
       (define px (@ p x))
       (define py (@ p y))
-      (and (<= l px)
-	   (<= t py)
-	   (< px r)
-	   (< py b)
+      (and (<= x1 px)
+	   (<= y1 py)
+	   (< px x2)
+	   (< py y2)
 	   this))))
 
 (define group-costume%
